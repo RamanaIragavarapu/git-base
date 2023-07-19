609f78a1d4e667fc28de88cf1c9a2b32a8846926
RamanaIragavarapu <iramana2001@gmail.com>
2023-07-19 20:54:36 +0530

    kdfj

---
+++ evenodd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Evenorodd
+{
+    static class EvenOdd
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter The Number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Given Number Is An Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Given Number Is Not An Even Number");
+            }
+        }
+    }
+}
---
+++ passorfail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace passfail
+{
+    class passFail
+    {
+        public static void Main(string[] args)
+        {
+            int m1 = 60, m2 = 32, m3 = 50;
+            if((m1>=50)&&(m2>=50)&&(m3>=50))
+            {
+                Console.WriteLine("pass");
+            }
+            else
+            {
+                Console.WriteLine("fail");
+            }
+        }
+    }
+}
